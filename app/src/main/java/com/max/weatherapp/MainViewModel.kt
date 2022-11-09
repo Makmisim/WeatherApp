a749832f7c608de4e05f8708b9481c06eb102b7c
Makmisim <mos141@mail.ru>
2022-11-09 16:45:03 +0300

    Add permission

---
+++ app/src/main/java/com/max/weatherapp/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.max.weatherapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<List<String>>()
+}
