4db075f755b2e25c7232c6c7cce248119e000a39
Makmisim <mos141@mail.ru>
2022-11-10 16:13:30 +0300

    Add VpAdapter, HoursFragment, DaysFragment

--- app/src/main/java/com/max/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/max/weatherapp/fragments/MainFragment.kt
@@ -9,10 +9,21 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.max.weatherapp.adapters.VpAdapter
 import com.max.weatherapp.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Часы",
+        "Дни"
+    )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -27,17 +38,28 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
     }
 
-   private fun permissionListener(){
-       pLauncher = registerForActivityResult(
-           ActivityResultContracts.RequestPermission()){
-           Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
-       }
-   }
+    private fun init() = with(binding) {
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp){
+            tab, pos -> tab.text = tList[pos]
+        }.attach()
 
-    private fun checkPermission(){
-        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+    }
+
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
